1a7c0710ae1e90bdabb412a049557847e55060df
Asixa <Asixa@foxmail.com>
2019-01-18 11:45:45 -0500

    IPR准备

--- ALightGPU/header/GLWindow.h
+++ ALightGPU/header/GLWindow.h
@@ -4,11 +4,25 @@
 #include <fstream>
 
 void Render();
-
+bool keyDown[256];
 inline void Resize(int width, int height)
 {
 	glutReshapeWindow(ImageWidth, ImageHeight);
 }
+
+
+void mainMenu(int i)
+{
+	printf("Command %d", i);
+	//key((unsigned char)i, 0, 0);
+}
+void InitMenus()
+{
+	glutCreateMenu(mainMenu);
+	glutAddMenuEntry("Reset block [1]", '1');
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
 
 inline void WindowsUpdate()
 {
@@ -24,6 +38,64 @@ inline void TimerProc(int id)
 	glutTimerFunc(1, TimerProc, 1);
 }
 
+void mouse(int button, int state, int x, int y)
+{
+	int mods;
+
+	// if (state == GLUT_DOWN)
+	// {
+	// 	buttonState |= 1 << button;
+	// }
+	// else if (state == GLUT_UP)
+	// {
+	// 	buttonState = 0;
+	// }
+	//
+	// mods = glutGetModifiers();
+	//
+	// if (mods & GLUT_ACTIVE_SHIFT)
+	// {
+	// 	buttonState = 2;
+	// }
+	// else if (mods & GLUT_ACTIVE_CTRL)
+	// {
+	// 	buttonState = 3;
+	// }
+	//
+	// ox = x;
+	// oy = y;
+	//
+	// if (displaySliders)
+	// {
+	// 	if (params->Mouse(x, y, button, state))
+	// 	{
+	// 		glutPostRedisplay();
+	// 		return;
+	// 	}
+	// }
+
+	glutPostRedisplay();
+}
+void motion(int x, int y)
+{
+	printf("MouseMove %d,%d", x, y);
+}
+
+void key(unsigned char key, int /*x*/, int /*y*/)
+{
+	switch (key)
+	{
+	}
+	keyDown[key] = true;
+
+	glutPostRedisplay();
+}
+
+void keyUp(unsigned char key, int /*x*/, int /*y*/)
+{
+	keyDown[key] = false;
+}
+
 inline void InitWindow(int argc, char** argv, unsigned int mode, int x_position, int y_position, int width, int heigth, const char * title)
 {
 	glutInit(&argc, argv);
@@ -34,6 +106,12 @@ inline void InitWindow(int argc, char** argv, unsigned int mode, int x_position,
 	glClearColor(1.0, 0.0, 1.0, 1.0);
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+	//InitMenus();
+	glutMouseFunc(mouse);
+	glutMotionFunc(motion);
+	glutKeyboardFunc(key);
+	glutKeyboardUpFunc(keyUp);
+
 
 	glutReshapeFunc(Resize);
 	glutTimerFunc(1, TimerProc, 1);
