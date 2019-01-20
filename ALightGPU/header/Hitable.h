c6efa1fb7258b6a8889338937af837872fec2e3e
Asixa <Asixa@foxmail.com>
2019-01-19 22:24:04 -0500

    继续

--- ALightGPU/header/Hitable.h
+++ ALightGPU/header/Hitable.h
@@ -9,6 +9,7 @@
 #define METAL 2
 #define DIELECTIRC 3
 
+class AABB;
 class Material;
 struct HitRecord
 {
@@ -30,6 +31,7 @@ public:
 	};
 	virtual ~GPUHitable() = default;
 	bool Hit(const Ray& r, float tmin, float tmax, HitRecord& rec, Material* materials);
+	bool bounding_box(float t0, float t1, AABB& box);
 };
 
 
@@ -173,4 +175,45 @@ __device__ inline bool GPUHitable::Hit(const Ray& r, float tmin, float tmax, Hit
 		}
 	}
 	return false;
+}
+
+inline bool GPUHitable::bounding_box(float t0, float t1, AABB& box)
+{
+}
+
+inline float ffmin(float a, float b) { return a < b ? a : b; }
+inline float ffmax(float a, float b) { return a > b ? a : b; }
+
+
+class AABB {
+public:
+	AABB() {}
+	AABB(const Vec3& a, const Vec3& b) { _min = a; _max = b; }
+
+	bool Hit(const Ray& r, float tmin, float tmax) const {
+		for (auto a = 0; a < 3; a++) {
+			const float t0 = ffmin((_min[a] - r.Origin()[a]) / r.Direction()[a],
+				(_max[a] - r.Origin()[a]) / r.Direction()[a]);
+			const float t1 = ffmax((_min[a] - r.Origin()[a]) / r.Direction()[a],
+				(_max[a] - r.Origin()[a]) / r.Direction()[a]);
+			tmin = ffmax(t0, tmin);
+			tmax = ffmin(t1, tmax);
+			if (tmax <= tmin)
+				return false;
+		}
+		return true;
+	}
+
+	Vec3 _min;
+	Vec3 _max;
+};
+
+inline AABB SurroundingBox(AABB box0, AABB box1) {
+	const Vec3 _small(fmin(box0._min.x(), box1._min.x()),
+		fmin(box0._min.y(), box1._min.y()),
+		fmin(box0._min.z(), box1._min.z()));
+	const Vec3 big(fmax(box0._max.x(), box1._max.x()),
+		fmax(box0._max.y(), box1._max.y()),
+		fmax(box0._max.z(), box1._max.z()));
+	return AABB(_small, big);
 }
